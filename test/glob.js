953c15974978787535efcc742f242f5b4af830a9
James Halliday <mail@substack.net>
2014-01-10 22:14:47 -0800

    passing array glob tests

--- test/glob.js
+++ test/glob.js
@@ -2,15 +2,26 @@ var bulk = require('../');
 var test = require('tape');
 
 test('string glob', function (t) {
-    t.plan(5);
+    t.plan(2);
     
-    var sections = bulk(__dirname + '/glob', 'render/*.js');
-    t.deepEqual(Object.keys(sections).sort(), [ 'render' ]);
-    t.deepEqual(Object.keys(sections.render).sort(), [ 'x' ]);
-    t.deepEqual(
-        Object.keys(sections.render.x).sort(),
-        [ 'oneoneone', 'twotwotwo' ]
-    );
-    t.equal(sections.render.x.oneoneone, 111);
-    t.equal(sections.render.x.twotwotwo, 222);
+    var res = bulk(__dirname + '/glob', 'render/*.js');
+    t.deepEqual(Object.keys(res).sort(), [ 'render' ]);
+    t.deepEqual(res.render, { x: { oneoneone: 111, twotwotwo: 222 } } );
+});
+
+test('array globs', function (t) {
+    t.plan(9);
+    
+    var res = bulk(__dirname + '/glob', [ 'render/*.js', 'data/**/*.js' ]);
+    t.deepEqual(Object.keys(res).sort(), [ 'data', 'render' ]);
+    t.deepEqual(res.render, { x: { oneoneone: 111, twotwotwo: 222 } } );
+    
+    t.equal(typeof res.data.cats, 'function');
+    t.equal(res.data.cats, res.data.cats.index);
+    t.equal(typeof res.data.dogs, 'function');
+    t.equal(res.data.dogs, res.data.dogs.index);
+    
+    t.deepEqual(res.data.cats.meow, { x: 555 });
+    t.equal(res.data.dogs.small.yip(3), 333);
+    t.equal(res.data.dogs.wolf.doge, 'wow');
 });
